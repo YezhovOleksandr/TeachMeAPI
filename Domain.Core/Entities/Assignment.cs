e021520e9a39a31ecb3e77942fe80305312297b1
Oleksandr.Yezhov <o.yezhov.lionwood@gmail.com>
2024-06-27 13:12:42 +0300

    Initial commit

---
+++ Domain.Core/Entities/Assignment.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Core.Entities;
+
+public class Assignment : BaseEntity
+{
+    public string? TopicName { get; set; } = string.Empty;
+    public string? Description { get; set; } = string.Empty;
+    public double Price { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+}
---
+++ Domain.Core/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Core.Entities;
+
+public class Category : BaseEntity
+{
+    public string CategoryName { get; set; }
+    
+    //nav props
+    public List<Assignment> Assignments { get; set; } = new List<Assignment>();
+}
